405216203e18ea21480218ff13c51364bc3ac242
年迈的老秋风Windy <cxcyhdyx@sina.com>
2017-12-08 04:35:53 -0500

    Linux Compilation Bug Fix
    Fix a bug that EasyCrossPlatform will fail to compile under Linux.

--- xmake.lua
+++ xmake.lua
@@ -14,6 +14,7 @@ target("easycrossplatform_s")
 			add_files("src/asm/x64/**.asm","src/asm/shared/**.asm")
 		end
 	else --Linux Distribution, xmake only supports x86 and x64
+		add_cxxflags("-std=c++11")
 		if is_arch("i386") then
 			add_defines("X86","EASYCROSSPLATFORM_ARCHITECTURE_X86")
 		elseif is_arch("x86_64") then
@@ -41,12 +42,15 @@ target("easycrossplatform")
 			add_files("src/asm/x64/**.asm","src/asm/shared/**.asm")
 		end
 	else --Linux Distribution, xmake only supports x86 and x64
+		add_cxxflags("-std=c++11")
 		if is_arch("i386") then
 			add_defines("X86","EASYCROSSPLATFORM_ARCHITECTURE_X86")
 		elseif is_arch("x86_64") then
 			add_defines("X64","EASYCROSSPLATFORM_ARCHITECTURE_X64")
 		end
 	end
-	
-	add_links("libmariadb")
+	if(is_plat("windows")) then
+		add_links("libmariadb")
+	end
+	add_rpathdirs("@loader_path/lib","@loader_path","@loader_path","@executable_path/lib","@executable_path")
 	
