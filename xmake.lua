2da03a633a1e8b0b9bc5c826e16789e3cc34c783
年迈的老秋风Windy <cxcyhdyx@sina.com>
2018-01-04 17:17:28 -0500

    Upgrade Make File
    The new version of makefile no longer require the transfer of .lib files. It will automatically detect the system type and choose the .lib file.

--- xmake.lua
+++ xmake.lua
@@ -4,30 +4,36 @@ target("easycrossplatform_s")
 	set_kind("static")
 	add_files("src/**.cpp","src/**.cc","src/**.c")
 	
-	add_includedirs("src","include")
+	add_includedirs("src","include","include/extlibs")
 	add_linkdirs("lib")
-	add_defines("CURL_STATICLIB")
+	add_defines("CURL_STATICLIB","SFML_STATIC")
 	if(is_plat("windows")) then --Windows, Only x86 and x64
 		if is_arch("x86") then
 			add_defines("_M_X86","EASYCROSSPLATFORM_ARCHITECTURE_X86")
 			add_files("src/asm/x86/**.asm","src/asm/shared/**.asm")
+			add_linkdirs("lib/libcurl/Win_x86","lib/mariadb/Win_x86")
 		elseif is_arch("x64") then
 			add_defines("_M_X64","EASYCROSSPLATFORM_ARCHITECTURE_X64")
 			add_files("src/asm/x64/**.asm","src/asm/shared/**.asm")
+			add_linkdirs("lib/libcurl/Win_x86","lib/mariadb/Win_x64")
 		end
 	else --Linux Distribution, xmake only supports x86 and x64
 		add_cxxflags("-std=c++11")
 		if is_arch("i386") then
 			add_defines("X86","EASYCROSSPLATFORM_ARCHITECTURE_X86")
+			add_linkdirs("lib/mariadb/Linux_x86")
 		elseif is_arch("x86_64") then
 			add_defines("X64","EASYCROSSPLATFORM_ARCHITECTURE_X64")
+			add_linkdirs("lib/mariadb/Linux_x64")
 		--ARMS EASYCROSSPLATFORM_ARCHITECTURE_ARM64 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv5 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv6 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv7 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv8
 		elseif is_arch("arm64*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARM64")
+			add_linkdirs("lib/mariadb/Linux_ARM64")
 		elseif is_arch("armv5*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARMv5")
 		elseif is_arch("armv6*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARMv6")
+			add_linkdirs("lib/mariadb/Linux_ARMv6")
 		elseif is_arch("armv7*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARMv7")
 		elseif is_arch("armv8*") then
@@ -49,30 +55,36 @@ target("easycrossplatform")
 	set_kind("shared")
 	add_files("src/**.cpp","src/**.cc","src/**.c")
 	
-	add_includedirs("src","include")
+	add_includedirs("src","include","include/extlibs")
 	add_linkdirs("lib")
 	
 	if(is_plat("windows")) then --Windows, Only x86 and x64
 		if is_arch("x86") then
 			add_defines("_M_X86","EASYCROSSPLATFORM_ARCHITECTURE_X86")
 			add_files("src/asm/x86/**.asm","src/asm/shared/**.asm")
+			add_linkdirs("lib/libcurl/Win_x86","lib/mariadb/Win_x86")
 		elseif is_arch("x64") then
 			add_defines("_M_X64","EASYCROSSPLATFORM_ARCHITECTURE_X64")
 			add_files("src/asm/x64/**.asm","src/asm/shared/**.asm")
+			add_linkdirs("lib/libcurl/Win_x64","lib/mariadb/Win_x64")
 		end
 	else --Linux Distribution, xmake only supports x86 and x64
 		add_cxxflags("-std=c++11")
 		if is_arch("i386") then
 			add_defines("X86","EASYCROSSPLATFORM_ARCHITECTURE_X86")
+			add_linkdirs("lib/mariadb/Linux_x86")
 		elseif is_arch("x86_64") then
 			add_defines("X64","EASYCROSSPLATFORM_ARCHITECTURE_X64")
+			add_linkdirs("lib/mariadb/Linux_x64")
 		--ARMS EASYCROSSPLATFORM_ARCHITECTURE_ARM64 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv5 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv6 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv7 / EASYCROSSPLATFORM_ARCHITECTURE_ARMv8
 		elseif is_arch("arm64*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARM64")
+			add_linkdirs("lib/mariadb/Linux_ARM64")
 		elseif is_arch("armv5*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARMv5")
 		elseif is_arch("armv6*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARMv6")
+			add_linkdirs("lib/mariadb/Linux_ARMv6")
 		elseif is_arch("armv7*") then
 			add_defines("EASYCROSSPLATFORM_ARCHITECTURE_ARMv7")
 		elseif is_arch("armv8*") then
@@ -87,6 +99,6 @@ target("easycrossplatform")
 		add_links("libcurl")
 	else
 		add_links("mariadb")
-		add_links("curl")
+		add_ldflags("-lcurl")
 	end
 	add_rpathdirs("@loader_path/lib","@loader_path","@loader_path","@executable_path/lib","@executable_path")
